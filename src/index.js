1210deccab13e029b1369f2ba6669d1c2dff4973
xcaio <mrlose22@gmail.com>
2024-09-09 13:39:47 -0300

    update port vercel

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import connectDatabase from './database/db.js'
 import route from './routes.js';
 import cors from 'cors'
 
+const port = process.env.PORT || 3001;
+
 app.use(express.json())
 app.use(cors())
 app.use(route)
